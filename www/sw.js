25087ecf14519947ebaee9dcfe4a7a9ca7c12d94
BK <gustavik@inbox.ru>
2026-04-13 23:13:12 +0300

    Delete www directory

--- www/sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE_NAME = 'bk-bank-v1';
-const urlsToCache = ['/','/index.html','/manifest.json'];
-
-self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
-self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).then(response => { const r = response.clone(); caches.open(CACHE_NAME).then(cache => cache.put(event.request, r)); return response; }).catch(() => caches.match(event.request))); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(names => Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))))); });
