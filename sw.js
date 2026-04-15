10735929b39e6959b5e9da07e1d8a5d689dea8c9
BK <gustavik@inbox.ru>
2026-04-16 00:25:27 +0300

    Update sw.js BK-sys

--- sw.js
+++ sw.js
@@ -1,4 +1,42 @@
 const CACHE_NAME = 'bk-bank-v2';
-self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['/','/index.html','/manifest.json']))); });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).then(r => { const rc = r.clone(); caches.open(CACHE_NAME).then(c => c.put(e.request, rc)); return r; }).catch(() => caches.match(e.request))); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(n => Promise.all(n.filter(nn => nn !== CACHE_NAME).map(nn => caches.delete(nn))))); });
+const urlsToCache = [
+  '/BK-Bank/',
+  '/BK-Bank/index.html',
+  '/BK-Bank/manifest.json',
+  '/BK-Bank/lib/crypto-js.min.js'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clonedResponse = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clonedResponse);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
